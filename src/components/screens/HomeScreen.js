663b0d5cbdd389b736dda93118a1ca3bc740ff39
fhuss009 <fhuss009@fiu.edu>
2020-05-03 21:54:43 -0500

    made component for screens

---
+++ src/components/screens/HomeScreen.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react'
+import ReactDOM from 'react-dom'
+
+class HomeScreen extends Component{
+    render (){
+        return (<h1>Welcome to your Home Screen</h1>);
+    }
+}
+
+export default HomeScreen
--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import HomeScreen from './components/screens/HomeScreen.js'
 
 const element = <h1>My first header!!</h1>;
 console.log(element);
 
-ReactDOM.render(element, document.getElementById('root'));
+ReactDOM.render(<HomeScreen/>, document.getElementById('root'));  
